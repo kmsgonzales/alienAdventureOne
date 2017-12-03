b35ebab4cca11210e591a3062cec7b43f31dc53e
Katherine-Marie Gonzales <kmsgonzales@comcast.net>
2017-12-02 23:27:48 -0800

    hands over least valuable item

--- Alien Adventure/LeastValuableItem.swift
+++ Alien Adventure/LeastValuableItem.swift
@@ -9,8 +9,19 @@
 extension Hero {
     
     func leastValuableItem(inventory: [UDItem]) -> UDItem? {
-        return nil
+        var leastValuable: UDItem?
+        if inventory.isEmpty {
+            return nil
+        } else {
+            leastValuable = inventory[0]
+            for item in inventory {
+                if item.baseValue < leastValuable!.baseValue {
+                    leastValuable = item
+                }
+        }
     }
+    return leastValuable
+}
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 4"
