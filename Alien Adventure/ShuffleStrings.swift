0d681f96365e002cea86dcc2e37ad886010b410b
Katherine-Marie Gonzales <kmsgonzales@comcast.net>
2017-12-03 21:04:58 -0800

    Robot reaches treasure

--- Alien Adventure/ShuffleStrings.swift
+++ Alien Adventure/ShuffleStrings.swift
@@ -9,6 +9,31 @@
 extension Hero {
     
     func shuffleStrings(s1: String, s2: String, shuffle: String) -> Bool {
-        return true
+        if s1.isEmpty && s2.isEmpty && shuffle.isEmpty {
+            return true
+        }
+        
+        if s1.characters.count + s2.characters.count != shuffle.characters.count {
+            return false
+        }
+        
+        var s1Shuffle = String()
+        var s2Shuffle = String()
+        
+        for n in shuffle.characters {
+            if s1.contains(String(n)) {
+                s1Shuffle.append(n)
+            }
+            if s2.contains(String(n)) {
+                s2Shuffle.append(n)
+            }
+        }
+        
+        if s1Shuffle == s1 && s2Shuffle == s2 {
+            return true
+        }
+        return false
     }
 }
+
+
